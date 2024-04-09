99081090b69a5c8c2cee599df4e01ea5cb146722
Sairam <deekondasairam017@gmail.com>
2024-04-09 13:59:55 +0530

    I added a method

--- learningGit.cpp
+++ learningGit.cpp
@@ -3,10 +3,16 @@
 #include <iostream>
 using namespace std;
 
+
+void add() {
+    cout << "Just added a new method";
+}
+
 int main()
 {
     cout << "Hello World!\n";
 
     cout << "Learning Github";
+
 }
 
